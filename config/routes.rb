be9ae4b6b8bc770cdbeeb50b8d1852d81acc2a23
Juan Diego <jddm11@gmail.com>
2016-02-16 20:14:03 -0300

    Task C:Catalog Display

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
+  get 'store/index'
+
   resources :products
+
+
+# The priority is based upon order of creation:
+# first created -> highest priority.
+# See how all your routes lay out with "rake routes".
+# You can have the root of your site routed with "root"
+ root 'store#index', as: 'store'
+
+# The priority is based upon order of cre# first created -> highest priority.
+# See how all your routes lay out with "r# You can have the root of your site rout root 'store#index', as: 'store'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
